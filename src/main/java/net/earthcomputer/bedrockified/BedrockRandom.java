a648c08151b6c5f6948eb8a379b855e5663079f2
Earthcomputer <burtonjae@hotmail.co.uk>
2018-11-30 16:10:39 +0000

    Fixes and documentation for BedrockRandom

--- src/main/java/net/earthcomputer/bedrockified/BedrockRandom.java
+++ src/main/java/net/earthcomputer/bedrockified/BedrockRandom.java
@@ -1,26 +1,113 @@
 package net.earthcomputer.bedrockified;
 
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
 import java.util.Random;
 
+/**
+ * An RNG implementation equivalent to that of Minecraft: Bedrock Edition.
+ *
+ * This is a version of the m19937 mersenne twister algorithm.
+ * This version contains the improved initialization from http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MT2002/CODES/mt19937ar.c
+ * It also contains an optimization to only generate some of the MT array when the RNG is first initialized.
+ *
+ * @author Earthcomputer
+ */
 public class BedrockRandom extends Random {
 
-    private int seed; // (DWORD*) this + 0
-    private int[] arr = new int[624]; // (DWORD*) this + 1
-    private int i; // (DWORD*) this + 625
-    private boolean haveNextNextGaussian; // (DWORD*) this + 626
-    private float nextNextGaussian; // (float*) this + 627
-    private int index; // (DWORD*) this + 628
-    private boolean valid = false;
-
-    private static int[] randomMTArray = {0, 0x9908b0df};
+    private static final int N = 624;
+    private static final int M = 327;
+    private static final int MATRIX_A = 0x9908b0df;
+    private static final int UPPER_MASK = 0x80000000;
+    private static final int LOWER_MASK = 0x7fffffff;
+    private static final int[] MAG_01 = {0, MATRIX_A};
     private static final double TWO_POW_M32 = 1.0 / (1L << 32);
 
+    private int seed; // (DWORD*) this + 0
+    private int[] mt = new int[N]; // (DWORD*) this + 1
+    private int mti; // (DWORD*) this + 625
+    private boolean haveNextNextGaussian; // (DWORD*) this + 626
+    private float nextNextGaussian; // (float*) this + 627
+    private int mtiFast; // (DWORD*) this + 628
+
+    private boolean valid = false; // Hackfix for setSeed being called too early in the superconstructor
+
+
+    public BedrockRandom() {
+        this(new Random().nextInt());
+    }
+
+    public BedrockRandom(int seed) {
+        valid = true;
+        _setSeed(seed);
+    }
+
+
+    // ===== PUBLIC INTERFACE METHODS ===== //
+
+    public int getSeed() {
+        return seed;
+    }
+
+    /**
+     * This overload exists to override the method in the base class.
+     * Although it accepts a 64-bit long, it will be cast to a 32-bit int.
+     */
     @Override
-    public double nextDouble() {
-        return _genRandReal2();
+    public void setSeed(long seed) {
+        if (valid) // Hackfix for this being called too early in the superconstructor
+            setSeed((int) seed);
+    }
+
+    public void setSeed(int seed) {
+        _setSeed(seed);
+    }
+
+    /**
+     * Generates a non-negative signed integer
+     */
+    @Override
+    public int nextInt() {
+        return _genRandInt32() >>> 1;
+    }
+
+    @Override
+    public int nextInt(int bound) {
+        if (bound > 0)
+            return Math.floorMod(_genRandInt32(), bound);
+        else
+            return 0;
+    }
+
+    /**
+     * Generates a random integer k such that a <= k < b
+     */
+    public int nextInt(int a, int b) {
+        if (a < b)
+            return a + nextInt(b - a);
+        else
+            return a;
+    }
+
+    /**
+     * Generates a random integer k such that a <= k <= b
+     */
+    public int nextIntInclusive(int a, int b) {
+        return nextInt(a, b + 1);
+    }
+
+    /**
+     * Generates a random long k such that 0 <= k < 2^32, mti.e. a random unsigned int
+     */
+    public long nextUnsignedInt() {
+        return Integer.toUnsignedLong(_genRandInt32());
+    }
+
+    /**
+     * Generates a random short k such that 0 <= k < 256, mti.e. a random unsigned byte
+     */
+    public short nextUnsignedChar() {
+        return (short) (_genRandInt32() & 0xff);
     }
 
     @Override
@@ -28,16 +115,44 @@ public class BedrockRandom extends Random {
         return (_genRandInt32() & 0x8000000) != 0;
     }
 
-    private void _initGenRand(int initialValue) {
-        this.arr[0] = initialValue;
-        for (this.i = 1; this.i < 624; this.i++) {
-            this.arr[i] = 1812433253
-                    * ((this.arr[this.i - 1] >>> 30) ^ this.arr[this.i - 1])
-                    + this.i;
-        }
-        this.index = 624;
+    /**
+     * Generates a uniform random float k such that 0 <= k < 1
+     */
+    @Override
+    public float nextFloat() {
+        return (float) _genRandReal2();
     }
 
+    /**
+     * Generates a uniform random float k such that 0 <= k < bound
+     */
+    public float nextFloat(float bound) {
+        return nextFloat() * bound;
+    }
+
+    /**
+     * Generates a uniform random float k such that a <= k < b
+     */
+    public float nextFloat(float a, float b) {
+        return a + (nextFloat() * (b - a));
+    }
+
+    /**
+     * Generates a uniform random double k such that 0 <= k < 1.
+     *
+     * Note that unlike the Java RNG, there are only 2^32 possible return values for this function
+     */
+    @Override
+    public double nextDouble() {
+        return _genRandReal2();
+    }
+
+    /**
+     * Generates a Gaussian distributed float with mean 0 and standard deviation 1.
+     *
+     * This method returns a double to override the method in the base class, but it is a
+     * float that's generated, and it can be safely cast back to float without loss of precision.
+     */
     @Override
     public double nextGaussian() {
         if (haveNextNextGaussian) {
@@ -52,120 +167,36 @@ public class BedrockRandom extends Random {
             s = v1 * v1 + v2 * v2;
         } while (s == 0 || s > 1);
 
-        float multiplier = MathHelper.sqrt(-2 * (float) Math.log(s) / s);
+        float multiplier = (float) Math.sqrt(-2 * (float) Math.log(s) / s);
         nextNextGaussian = v2 * multiplier;
         haveNextNextGaussian = true;
         return v1 * multiplier;
     }
 
-    private int _genRandInt32() {
-        if (this.i < 625) {
-            if (this.i == 624) {
-                this.i = 0;
-            }
-        } else {
-            _initGenRand(5489);
-            this.i = 0;
-        }
-        if (this.i >= 227) {
-            if (this.i >= 623) {
-                this.arr[623] = randomMTArray[this.arr[0] & 1]
-                        ^ ((this.arr[0] & 0x7fffffff | this.arr[623] & 0x80000000) >>> 1)
-                        ^ this.arr[396];
-            } else {
-                this.arr[this.i] = randomMTArray[this.arr[this.i + 1] & 1]
-                        ^ ((this.arr[this.i + 1] & 0x7fffffff | this.arr[this.i] & 0x80000000) >>> 1)
-                        ^ this.arr[this.i - 227];
-            }
-        } else {
-            this.arr[this.i] = randomMTArray[this.arr[this.i + 1] & 1]
-                    ^ ((this.arr[this.i + 1] & 0x7fffffff | this.arr[this.i] & 0x80000000) >>> 1)
-                    ^ this.arr[this.i + 397];
-            if (this.index < 624) {
-                this.arr[this.index] = 1812433253
-                        * ((this.arr[this.index - 1] >> 30) ^ this.arr[this.index - 1])
-                        + this.index;
-                this.index++;
-            }
-        }
-        int v1 = i++;
-        int v2 = this.arr[v1];
-        int v3 = ((v2 ^ (v2 >>> 11)) << 7) & 0x9d2c5680 ^ v2 ^ (v2 >>> 11);
-        return (v3 << 15) & 0xefc60000 ^ v3 ^ (((v3 << 15) & 0xefc60000 ^ v3) >>> 18);
-    }
-
-    private double _genRandReal2() {
-        return Integer.toUnsignedLong(_genRandInt32()) * TWO_POW_M32;
-    }
-
+    /**
+     * Returns a triangularly distributed int k with mode 0 such that -bound < k < bound.
+     *
+     * Note that the method name, used in the Bedrock code, incorrectly describes this as a Gaussian distribution.
+     */
     public int nextGaussianInt(int bound) {
         return nextInt(bound) - nextInt(bound);
     }
 
-    public long nextUnsignedInt() {
-        return Integer.toUnsignedLong(_genRandInt32());
-    }
-
-    private void _initGenRandFast(int initialValue) {
-        this.arr[0] = initialValue;
-        int v4 = initialValue;
-        for (int i = 1; i < 398; i++) {
-            this.arr[i] = 1812433253 * ((v4 >>> 30) ^ v4) + i;
-            v4 = 1812433253 * ((v4 >>> 30) ^ v4) + i;
-        }
-        this.i = 624;
-        this.index = 398;
-    }
-
-    public Vec3d nextGaussianVec3() {
-        float x = (float) nextGaussian();
-        float y = (float) nextGaussian();
-        float z = (float) nextGaussian();
-        return new Vec3d(x, y, z);
-    }
-
-    public int nextIntInclusive(int a, int b) {
-        return nextInt(a, b + 1);
-    }
-
-    public byte nextUnsignedChar() {
-        return (byte) _genRandInt32();
-    }
-
+    /**
+     * Returns a triangularly distributed float k with mode 0 such that -1 < k < 1.
+     *
+     * Note that the method name, used in the Bedrock code, incorrectly describes this as a Gaussian distribution.
+     */
     public float nextGaussianFloat() {
         return nextFloat() - nextFloat();
     }
 
-    @Override
-    public int nextInt(int bound) {
-        if (bound > 0)
-            return _genRandInt32() % bound;
-        else
-            return 0;
-    }
-
-    public int nextInt(int a, int b) {
-        if (a < b)
-            return a + nextInt(b - a);
-        else
-            return a;
-    }
-
-    @Override
-    public int nextInt() {
-        return _genRandInt32() >>> 1;
-    }
-
-    @Override
-    public void setSeed(long seed) {
-        if (valid) // Hackfix for this being called too early in the superconstructor
-            setSeed((int) seed);
-    }
-
-    public void setSeed(int seed) {
-        _setSeed(seed);
-    }
-
+    /**
+     * Returns uniformly distributed 3d float vector v such that 0 <= v_i < 1 for each mti.
+     *
+     * Note that although this method returns a double vector, the precision is only float,
+     * and can be safely cast back to floats without loss of precision.
+     */
     public Vec3d nextVec3() {
         float x = nextFloat();
         float y = nextFloat();
@@ -173,38 +204,89 @@ public class BedrockRandom extends Random {
         return new Vec3d(x, y, z);
     }
 
+    /**
+     * Returns a float vector containing 3 Gaussian distributed floats with mean 0 and standard deviation 1.
+     *
+     * Note that although this method returns a double vector, the precision is only float,
+     * and can be safely cast back to floats without loss of precision.
+     */
+    public Vec3d nextGaussianVec3() {
+        float x = (float) nextGaussian();
+        float y = (float) nextGaussian();
+        float z = (float) nextGaussian();
+        return new Vec3d(x, y, z);
+    }
+
+
+    // ===== m19937 MERSENNE TWISTER IMPLEMENTATION ===== //
+
     private void _setSeed(int seed) {
         this.seed = seed;
-        this.i = 625;
+        this.mti = N + 1; // uninitialized
         this.haveNextNextGaussian = false;
         this.nextNextGaussian = 0;
         _initGenRandFast(seed);
     }
 
-    public float nextFloat(float bound) {
-        return nextFloat() * bound;
+    private void _initGenRand(int initialValue) {
+        this.mt[0] = initialValue;
+        for (this.mti = 1; this.mti < N; this.mti++) {
+            this.mt[mti] = 1812433253
+                    * ((this.mt[this.mti - 1] >>> 30) ^ this.mt[this.mti - 1])
+                    + this.mti;
+        }
+        this.mtiFast = N;
     }
 
-    public float nextFloat(float a, float b) {
-        return a + (nextFloat() * (b - a));
+    private void _initGenRandFast(int initialValue) {
+        this.mt[0] = initialValue;
+        for (this.mtiFast = 1; this.mtiFast <= M; this.mtiFast++) {
+            this.mt[this.mtiFast] = 1812433253
+                    * ((this.mt[this.mtiFast - 1] >>> 30) ^ this.mt[this.mtiFast - 1])
+                    + this.mtiFast;
+        }
+        this.mti = N;
     }
 
-    @Override
-    public float nextFloat() {
-        return (float) _genRandReal2();
+    private int _genRandInt32() {
+        if (this.mti == N) {
+            this.mti = 0;
+        } else if (this.mti > N) {
+            _initGenRand(5489);
+            this.mti = 0;
+        }
+
+        if (this.mti >= N - M) {
+            if (this.mti >= N - 1) {
+                this.mt[N - 1] = MAG_01[this.mt[0] & 1]
+                        ^ ((this.mt[0] & LOWER_MASK | this.mt[N - 1] & UPPER_MASK) >>> 1)
+                        ^ this.mt[M - 1];
+            } else {
+                this.mt[this.mti] = MAG_01[this.mt[this.mti + 1] & 1]
+                        ^ ((this.mt[this.mti + 1] & LOWER_MASK | this.mt[this.mti] & UPPER_MASK) >>> 1)
+                        ^ this.mt[this.mti - (N - M)];
+            }
+        } else {
+            this.mt[this.mti] = MAG_01[this.mt[this.mti + 1] & 1]
+                    ^ ((this.mt[this.mti + 1] & LOWER_MASK | this.mt[this.mti] & UPPER_MASK) >>> 1)
+                    ^ this.mt[this.mti + M];
+
+            if (this.mtiFast < N) {
+                this.mt[this.mtiFast] = 1812433253
+                        * ((this.mt[this.mtiFast - 1] >>> 30) ^ this.mt[this.mtiFast - 1])
+                        + this.mtiFast;
+                this.mtiFast++;
+            }
+        }
+
+        int ret = this.mt[this.mti++];
+        ret = ((ret ^ (ret >>> 11)) << 7) & 0x9d2c5680 ^ ret ^ (ret >>> 11);
+        ret = (ret << 15) & 0xefc60000 ^ ret ^ (((ret << 15) & 0xefc60000 ^ ret) >>> 18);
+        return ret;
     }
 
-    public BedrockRandom(int seed) {
-        valid = true;
-        _setSeed(seed);
-    }
-
-    public BedrockRandom() {
-        this(new Random().nextInt());
-    }
-
-    public int getSeed() {
-        return seed;
+    private double _genRandReal2() {
+        return Integer.toUnsignedLong(_genRandInt32()) * TWO_POW_M32;
     }
 
 }
